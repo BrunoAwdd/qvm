1faedfdfe53ee8e9038c18f4da81091aa16c8cf6
Bruno <bruno@awdd.com.br>
2025-04-09 15:12:13 -0300

    feature: Add new gates

--- qvm/src/gates/mod.rs
+++ qvm/src/gates/mod.rs
@@ -6,13 +6,22 @@ pub mod one_q{
     pub mod pauli_x;
     pub mod pauli_y;
     pub mod pauli_z;
-    pub mod rx;
-    pub mod ry;
-    pub mod rz;
     pub mod s;
     pub mod s_dagger;
     pub mod t;
     pub mod t_dagger;
+
+}
+
+pub mod general {
+    pub mod controlled_u;
+}
+
+pub mod rotation_q {
+    pub mod phase;
+    pub mod rx;
+    pub mod ry;
+    pub mod rz;
     pub mod u1;
     pub mod u2;
     pub mod u3;
@@ -20,6 +29,8 @@ pub mod one_q{
 
 pub mod two_q{
     pub mod cnot;
+    pub mod cy;
+    pub mod cz;
     pub mod swap;
 }
 
